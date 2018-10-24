d315ab91b38439854991a641c02d13b37088c5dc
LAPTOP-T8B6645U\santh <santhidanta99@gmail.com>
2018-10-23 19:37:06 -0500

    modify the testcase

--- src/test/java/com/academy/Homepage.java
+++ src/test/java/com/academy/Homepage.java
@@ -17,5 +17,8 @@ public class Homepage extends Baseclass{
 	@Test
 	public void uhbimimumhi() {
 		
+		driver.get("http://www.qaclickacademy.com/");
+		Landingpage lp= new Landingpage(driver);
+		lp.Login().click();
 	}
 }
